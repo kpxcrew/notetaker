ff0245425631ba0f531cb9ee3c6d78df5e64e66a
kpxcrew <118790881+kpxcrew@users.noreply.github.com>
2023-05-07 21:05:40 -0500

    added server data, updated pakcage.json, readded given code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path')
+const fs = require('fs')
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: true })); 
+
+
+app.use(express.static('public'));
+
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
